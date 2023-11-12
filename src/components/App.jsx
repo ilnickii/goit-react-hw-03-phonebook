29efeac043adfc9ce4430bf05522669ec7caef96
ilnickii <ilnickii2008@gmail.com>
2023-11-12 16:39:34 +0300

    added save to localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,6 +16,22 @@ export class App extends Component {
     filter: '',
   };
 
+
+   componentDidMount() {
+    const savedContacts = localStorage.getItem('contacts');
+    if (savedContacts) {
+      this.setState({ contacts: JSON.parse(savedContacts) });
+    }
+  }
+
+
+   componentDidUpdate(_, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
+
   handleAddContact = ( name, number) => {
     const isNameExists = this.state.contacts.some(
       contact => contact.name.toLowerCase() === name.toLowerCase()
